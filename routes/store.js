9347b01f00d21ef3f18a1c3f1c79ced638377e92
45183 <hsb9743@naver.com>
2024-02-01 11:19:38 +0900

    2/1

--- routes/store.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const db = require('../config/db');
-
-router.get('/', async(req, res, next) => {
-    db.connection.query('select * from products')
-})
