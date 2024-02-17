164efdf1821b3b6496a71210e1c6f0a139ebdd90
nayeolsimy <nayeolsimy@gmail.com>
2024-02-17 15:09:05 +0900

    feat: userId 수정

--- controller/authController.js
+++ controller/authController.js
@@ -1,17 +1,25 @@
-const passport = require('passport');
+const passport = require("passport");
 const response = require("../data/responseFrom");
 const resTEXT = require("../data/responseString");
 
 exports.login = (req, res, next) => {
-    passport.authenticate('local', (authError, user, info) => {
-        if (user) req.login(user, loginError =>
-            res.status(200).json(response.responseFromData(resTEXT.RESPONSE_TEXT.SUCCESS, resTEXT.AUTH_MESSAGE.SUCCESS, {
-                userInfo :{
-                    userid: user.id,
-                    userName: user.name,
-                    userDescription: user.description
-                }
-            })));
-        else next(info);
-    })(req, res, next);
+	passport.authenticate("local", (authError, user, info) => {
+		if (user)
+			req.login(user, (loginError) =>
+				res.status(200).json(
+					response.responseFromData(
+						resTEXT.RESPONSE_TEXT.SUCCESS,
+						resTEXT.AUTH_MESSAGE.SUCCESS,
+						{
+							userInfo: {
+								userId: user.userId,
+								userName: user.name,
+								userDescription: user.description,
+							},
+						}
+					)
+				)
+			);
+		else next(info);
+	})(req, res, next);
 };
--- repository/userRepository.js
+++ repository/userRepository.js
@@ -1,10 +1,15 @@
-const User = require('../models/user');
+const User = require("../models/user");
 
+exports.findUserByEmail = (email) => User.findOne({ where: { email } });
 
-exports.findUserByEmail = (email) => User.findOne({where: {email}});
+exports.createUser = (email, name, password, phone) =>
+	User.create({
+		email,
+		name,
+		password,
+		phone,
+		permission: false,
+	});
 
-exports.createUser = (email, name, password, phone) => User.create({
-    email, name, password, phone, permission: false
-});
-
-exports.findAllUser = () => User.findAll({attributes: ['email', 'name', 'phone']});
+exports.findAllUser = () =>
+	User.findAll({ attributes: ["email", "name", "phone"] });
