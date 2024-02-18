e7fe284b54fc8a0f8cefdc272e6afcbed575fc1c
nayeolsimy <nayeolsimy@gmail.com>
2024-02-18 21:04:45 +0900

    feat: session에  userId 저장

--- passport/index.js
+++ passport/index.js
@@ -1,19 +1,23 @@
-const passport = require('passport');
-const local = require('./local');
-const kakao = require('./kakao');
-const userRepository = require('../repository/userRepository')
+const passport = require("passport");
+const local = require("./local");
+const kakao = require("./kakao");
+const userRepository = require("../repository/userRepository");
 
 module.exports = () => {
-    passport.serializeUser((user, done) => {
-        done(null, user.email);
-    });
+	passport.serializeUser((user, done) => {
+		done(null, user.userId);
+	});
 
-    passport.deserializeUser((email, done) => {
-        userRepository.findUserByEmail(email)
-            .then(user => done(null, user))
-            .catch(err => done(err));
-    });
+	passport.deserializeUser((userId, done) => {
+		userRepository
+			.findUserByUserId(userId)
+			.then((user) => {
+				console.log("deserializeUser");
+				done(null, user);
+			})
+			.catch((err) => done(err));
+	});
 
-    local();
-    // kakao();
+	local();
+	// kakao();
 };
